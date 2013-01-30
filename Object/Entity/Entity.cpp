9276671d992bde5ae3801027e06f680c6c23c610
Mehdi MHIRI <mehdi.mhiri@gmail.com>
2013-01-30 03:58:46 +0100

    "Ghost" & prediction
    Now draws the "ghost" of an entity.
    Simplified prediction of the next location of an entity.

--- Object/Entity/Entity.cpp
+++ Object/Entity/Entity.cpp
@@ -17,30 +17,35 @@ Entity::Entity(const vec2f &Location){
 
 void Entity::Create(){}
 
+Entity NextLocation;
+
 void Entity::Tick(){
 	Velocity+=Acceleration;
 	Velocity.y+=Gravity;
 	if(fabs(Velocity.x)>MaxSpeed) Velocity.x=sign(Velocity.x)*MaxSpeed;
-    if(fabs(Velocity.y) > MaxSpeed) Velocity.y=sign(Velocity.y)*MaxSpeed;
-	if (Velocity.y!=0){
+    if(fabs(Velocity.y)>MaxSpeed) Velocity.y=sign(Velocity.y)*MaxSpeed;
+    NextLocation.GridLocation=GridLocation;
+    NextLocation.GridLocation.x+=floor(Velocity.x/16+0.5);
+    NextLocation.GridLocation.y+=floor(Velocity.y/16+0.5);
+    if(Velocity.x!=0){
+		int Sign=sign(Velocity.x);
+		int Block=Level->GetBlockAt(NextLocation.GridLocation+vec2i(Sign,0));
+		if(Block>0){
+			Velocity.x=0;
+			Location.x=(GridLocation.x)*16;
+		}
+	}
+	if(Velocity.y!=0){
 		int Sign=sign(Velocity.y);
-		int Block=Level->GetBlockAt(GridLocation+vec2i(0,Sign));
+		int Block=Level->GetBlockAt(NextLocation.GridLocation+vec2i(0,Sign));
 		if(Block>0){
 			Velocity.y=0;
 			Location.y=(GridLocation.y)*16;
 		}
 	}
-    if (Velocity.x!=0){
-		int Sign=sign(Velocity.x);
-		int Block=Level->GetBlockAt(GridLocation+vec2i(Sign,0));
-		if (Block >0){
-			Velocity.x=0;
-			Location.x=(GridLocation.x)*16;
-		}
-	}
 	Location+=Velocity;
-	GridLocation.x=floor(Location.x/16);
-	GridLocation.y=floor(Location.y/16);
+	GridLocation.x=floor(Location.x/16+0.5);
+	GridLocation.y=floor(Location.y/16+0.5);
 }
 
 void Entity::Draw(){
@@ -50,6 +55,7 @@ void Entity::Draw(){
 		glVertex3f(Location.x+16,Location.y+16,Layer);
 		glVertex3f(Location.x,Location.y+16,Layer);
 	glEnd();
+	WireDraw();
 }
 
 void Entity::WireDraw(){
